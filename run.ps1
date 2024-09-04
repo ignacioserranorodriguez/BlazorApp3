4b896a9b7dcd72d0febae6c7fbad4caeb079008a
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-09-04 15:34:31 +0200

    Powershell script added to manage the web server and the mysql container
    Blazor web app is exposed in the port 5049

---
+++ run.ps1
@@ -0,0 +1,34 @@
+# Navigate to the project directory
+Set-Location -Path "$PSScriptRoot"
+
+# Function to stop and remove the containers
+function Stop-DockerCompose {
+    Write-Host "Stopping and removing Docker containers..."
+    docker-compose down
+}
+
+# Register an event handler for Ctrl+C (SIGINT)
+$eventHandler = {
+    Stop-DockerCompose
+    exit
+}
+$null = Register-EngineEvent -SourceIdentifier ConsoleBreak -Action $eventHandler
+
+try {
+    # Run docker-compose up command
+    docker-compose up -d
+
+    # Update-Database from NuGet Package Manager Console
+    dotnet ef database update
+
+    # Run the Blazor web app in the foreground
+    Set-Location -Path "$PSScriptRoot\BlazorApp3"
+    dotnet run --project BlazorApp3.csproj
+}
+catch {
+    Write-Host "An error occurred: $_"
+}
+finally {
+    # Ensure Docker containers are stopped
+    Stop-DockerCompose
+}
