df15a089eb98a35799d7ebbe11ab5a594ff3302e
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-09-15 21:01:56 +0200

    Default profile loaded when the browser is launched

--- mock_data.sql
+++ mock_data.sql
@@ -16,7 +16,7 @@ INSERT INTO `Profile` (`Id`, `Name`) VALUES
 
 -- Insert data into Connection table
 INSERT INTO `Connection` (`Id`, `TransmitterId`, `ReceiverId`, `ProfileId`) VALUES
-(1, 1, 1, 1),
-(2, 1, 2, 1),
-(3, 2, 3, 1),
-(4, 2, 4, 1);
+(1, 1, 5, 1),
+(2, 1, 6, 1),
+(3, 2, 7, 1),
+(4, 2, 8, 1);
