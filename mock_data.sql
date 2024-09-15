f48449cd9790469dbf50ad4357b37eab7d32f7a0
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-09-15 19:50:39 +0200

    Mock data changed to use profiles

--- mock_data.sql
+++ mock_data.sql
@@ -4,20 +4,19 @@ INSERT INTO `Device` (`Id`, `Name`, `Ip`, `Status`, `Discriminator`, `Transmitte
 (2, 'Fedora 1', NULL, 1, 'Transmitter', NULL, 'https://upload.wikimedia.org/wikipedia/commons/8/82/Fedora_Workstation_40.png'),
 (3, 'Arch 1', NULL, 1, 'Transmitter', NULL, 'https://upload.wikimedia.org/wikipedia/commons/6/61/Arch_Linux_ARM-Asahi_Linux_Desktop_English_07-2023.png'),
 (4, 'WindowsXP 1', NULL, 1, 'Transmitter', NULL, 'https://upload.wikimedia.org/wikipedia/en/6/64/Windows_XP_Luna.png'),
-(5, 'Receptor 1', NULL, 1, 'Receiver', 1, NULL),
-(6, 'Receptor 2', NULL, 1, 'Receiver', 1, NULL),
-(7, 'Receptor 3', NULL, 1, 'Receiver', 2, NULL);
+(5, 'Receptor 1', NULL, 1, 'Receiver', NULL, NULL),
+(6, 'Receptor 2', NULL, 1, 'Receiver', NULL, NULL),
+(7, 'Receptor 3', NULL, 1, 'Receiver', NULL, NULL),
+(8, 'Receptor 4', NULL, 1, 'Receiver', NULL, NULL);
 
 -- Insert data into Profile table
 INSERT INTO `Profile` (`Id`, `Name`) VALUES
-(1, 'Profile 1'),
-(2, 'Profile 2');
+(1, 'Por defecto'),
+(2, 'Perfil 2');
 
 -- Insert data into Connection table
 INSERT INTO `Connection` (`Id`, `TransmitterId`, `ReceiverId`, `ProfileId`) VALUES
-(1, 1, 5, 1),
-(2, 1, 6, 1),
-(3, 2, 7, 1),
-(4, 3, 5, 2),
-(5, 4, 6, 2),
-(6, 2, 7, 2);
+(1, 1, 1, 1),
+(2, 1, 2, 1),
+(3, 2, 3, 1),
+(4, 2, 4, 1);
